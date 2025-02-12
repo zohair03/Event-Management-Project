a9ceec243c05eea774b76d21af91be85dca906af
zohair03 <142157237+zohair03@users.noreply.github.com>
2025-02-13 00:37:14 +0530

    data send test

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
+import {User} from "./models/userModel.js";
 
 dotenv.config({ path: "./.env" });
 
@@ -18,8 +19,15 @@ const PORT = process.env.PORT || 3001;
 
 const msg = "frontend and backend connected !!";
 
-app.get("/landingPage", (req, res) => {
-  res.send(msg);
+app.get("/landingPage", async (req, res) => {
+  const { name, username, password, emailId } = req.body;
+
+  try {
+    const users = await User.create({ name, username, password, emailId });
+    res.send(200).json(users);
+  } catch (error) {
+    res.send(400).json(error);
+  }
 });
 
 mongoose
