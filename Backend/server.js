2b16ff767382179a8314ce89a149d17c92fd58d7
zohair03 <142157237+zohair03@users.noreply.github.com>
2025-02-12 23:14:27 +0530

    Database connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
+import mongoose from "mongoose";
 
-dotenv.config({path: './.env'})
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
-app.use(cors(
-    {
-        origin: process.env.CORS_ORIGIN,
-        credentials: true
-    }
-))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3001;
 
@@ -21,6 +22,13 @@ app.get("/landingPage", (req, res) => {
   res.send(msg);
 });
 
-app.listen(PORT, () => {
-  console.log(`server is live at port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`MongoDB connected & server is live at port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error: ", error);
+  });
