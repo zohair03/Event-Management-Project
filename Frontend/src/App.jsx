3df080def617987b513b122d490b6e3648cbb834
zohair03 <142157237+zohair03@users.noreply.github.com>
2025-02-16 23:39:37 +0530

    router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,82 +6,39 @@ import Dashboard from "./components/Dashboard";
 import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
 import UpdateEvent from "./components/UpdateEvent";
-import {createBrowserRouter , RouterProvider} from 'react-router'
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 function App() {
-  
-  const [newUser, setNewUser] = useState({
-    name: "",
-    userName: "",
-    password: "",
-    email: ""
-  });
-
-  const [newEvent, setNewEvent] = useState({
-    name:"",
-    location:"",
-    description:""
-  })
-
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    // console.log({newUser})
-
-    axios
-      .post("http://localhost:3000/api/event/createEvent", newEvent)
-      .then((res) => {
-        console.log("data sended succesfully ", newEvent);
-        setNewEvent({ name: "", location: "", description: ""});
-      })
-      .catch((err) => {
-        console.log("error in sending data to backend ", err);
-      });
-  }
-
-  function handleChange(event) {
-    const { name, value } = event.target;
-    setNewEvent({
-      ...newEvent,
-      [name]: value,
-    });
-  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/loginPage",
+      element: <LoginPage />,
+    },
+    {
+      path: "/dashboard",
+      element: <Dashboard />,
+    },
+    {
+      path: "/createEvent",
+      element: <CreateEvent />,
+    },
+    {
+      path: "/updateEvent",
+      element: <UpdateEvent />,
+    },
+  ]);
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>
-          name
-          <input
-            type="text"
-            name="name"
-            value={newEvent.name}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          location
-          <input
-            type="text"
-            name="location"
-            value={newEvent.location}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          description
-          <input
-            type="text"
-            name="description"
-            value={newEvent.description}
-            onChange={handleChange}
-          />
-        </label>
-        <button type="submit">submit</button>
-      </form>
-    </>
-  );
+
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+    
+  )
 }
 
 export default App;
